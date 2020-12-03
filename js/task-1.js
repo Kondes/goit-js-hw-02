548e321652bd94c9edc4508e40ccd6a2e33e07cb
Andrii Kondysiuk <kondysiuka@gmail.com>
2020-12-03 00:34:02 +0000

    task1

--- js/task-1.js
+++ js/task-1.js
@@ -16,3 +16,14 @@
 // logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
 
 // logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+const elements = ['Mango', 'Poly', 'Ajax'];
+const logItems = function (array) {
+  for (let i = 0; i < array.length; i++) {
+    const message = `${i + 1} - ${array[i]}`;
+    console.log(message);
+  }
+};
+logItems(elements);
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
